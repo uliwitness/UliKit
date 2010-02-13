bc827a2762034add63a1cda21ce3dd8d42a34850
Uli Kusterer <ulivc@zathras.de>
2010-02-13 17:17:56 +0100

    Added Matt Gallagher's SynthesizeSingleton macro to UliKit.

---
+++ SynthesizeSingleton.h
@@ -0,0 +1,60 @@
+//
+//  SynthesizeSingleton.h
+//
+//  Created by Matt Gallagher on 20/10/08.
+//
+
+#define SYNTHESIZE_SINGLETON_FOR_CLASS(classname) \
+ \
+static classname *shared##classname = nil; \
+ \
++ (classname *)shared##classname \
+{ \
+	@synchronized(self) \
+	{ \
+		if (shared##classname == nil) \
+		{ \
+			[[self alloc] init]; \
+		} \
+	} \
+	 \
+	return shared##classname; \
+} \
+ \
++ (id)allocWithZone:(NSZone *)zone \
+{ \
+	@synchronized(self) \
+	{ \
+		if (shared##classname == nil) \
+		{ \
+			shared##classname = [super allocWithZone:zone]; \
+			return shared##classname; \
+		} \
+	} \
+	 \
+	return nil; \
+} \
+ \
+- (id)copyWithZone:(NSZone *)zone \
+{ \
+	return self; \
+} \
+ \
+- (id)retain \
+{ \
+	return self; \
+} \
+ \
+- (NSUInteger)retainCount \
+{ \
+	return NSUIntegerMax; \
+} \
+ \
+- (void)release \
+{ \
+} \
+ \
+- (id)autorelease \
+{ \
+	return self; \
+}

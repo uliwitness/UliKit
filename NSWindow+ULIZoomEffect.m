31810b6015d075bfacfdf3faa71bc1b176f69b35
Uli Kusterer <ulivc@zathras.de>
2011-03-05 19:05:25 +0100

    Added license, removed ifdefed-out code.

--- NSWindow+ULIZoomEffect.m
+++ NSWindow+ULIZoomEffect.m
@@ -5,6 +5,25 @@
 //  Created by Uli Kusterer on 05.03.11.
 //  Copyright 2011 Uli Kusterer. All rights reserved.
 //
+//	This software is provided 'as-is', without any express or implied
+//	warranty. In no event will the authors be held liable for any damages
+//	arising from the use of this software.
+//
+//	Permission is granted to anyone to use this software for any purpose,
+//	including commercial applications, and to alter it and redistribute it
+//	freely, subject to the following restrictions:
+//
+//	   1. The origin of this software must not be misrepresented; you must not
+//	   claim that you wrote the original software. If you use this software
+//	   in a product, an acknowledgment in the product documentation would be
+//	   appreciated but is not required.
+//
+//	   2. Altered source versions must be plainly marked as such, and must not be
+//	   misrepresented as being the original software.
+//
+//	   3. This notice may not be removed or altered from any source
+//	   distribution.
+//
 
 #import "NSWindow+ULIZoomEffect.h"
 
@@ -33,7 +52,6 @@
 
 -(NSImage*)	uli_imageWithSnapshotForceActive: (BOOL)doForceActive
 {
-#if 1
 	NSDisableScreenUpdates();
 	BOOL	wasVisible = [self isVisible];
 	
@@ -64,13 +82,6 @@
     CGImageRelease(windowImage);
     
     return [image autorelease];
-#else
-	[self display];
-	NSRect			boundsBox = [self frame];
-	boundsBox.origin = NSZeroPoint;
-    NSData		*   pdfData = [self dataWithPDFInsideRect: boundsBox];
-    return [[[NSImage alloc] initWithData: pdfData] autorelease];
-#endif
 }
 
 

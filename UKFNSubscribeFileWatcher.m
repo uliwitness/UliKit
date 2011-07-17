fc9d966d6b5c4f98324690ac9e8452b66b79605e
Matthieu Cormier <mcormier@cs.dal.ca>
2011-07-16 22:59:50 -0300

    Remove warning comparison always false due to limited range of data type

--- UKFNSubscribeFileWatcher.m
+++ UKFNSubscribeFileWatcher.m
@@ -229,7 +229,7 @@ void    UKFileSubscriptionProc(FNMessage message, OptionBits flags, void *refcon
 -(void) sendDelegateMessage: (FNMessage)message forSubscription: (FNSubscriptionRef)subscription
 {
     NSValue*                    subValue = [NSValue valueWithPointer: subscription];
-	int							idx = [subscribedObjects indexOfObject: subValue];
+	NSInteger						idx = [subscribedObjects indexOfObject: subValue];
 	if( idx == NSNotFound )
 	{
 		NSLog( @"Notification for unknown subscription." );

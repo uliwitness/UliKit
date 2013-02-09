ed965124c0b97a9e9d29d0c5fd880de7f1232c0b
Uli Kusterer <ulivc@zathras.de>
2013-02-09 19:00:29 +0100

    Use /usr/bin/git instead of some /usr/local/bin/whatever...

--- make_git_version_header.sh
+++ make_git_version_header.sh
@@ -40,8 +40,9 @@
 
 echo -n "note: Finding revision in "
 pwd
-revnum=`/usr/local/git/bin/git rev-list HEAD | /usr/bin/wc -l | tr -d ' '`
-fullrevnum=`/usr/local/git/bin/git rev-parse HEAD`
+GIT='/usr/bin/git'
+revnum=`$GIT rev-list HEAD | /usr/bin/wc -l | tr -d ' '`
+fullrevnum=`$GIT rev-parse HEAD`
 builddate=`date "+%Y-%m-%d"`
 
 # Now write the constant declaration to the file:

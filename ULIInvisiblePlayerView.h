c413be1ce57624843ca87279b3aab8dff730bc21
Uli Kusterer <ulivc@zathras.de>
2014-01-09 01:42:05 +0100

    Class like AVPlayerView, but suitable for older OSes (back to 10.7, where we first got AVFoundation), and without the annoying playback overlays.

---
+++ ULIInvisiblePlayerView.h
@@ -0,0 +1,17 @@
+//
+//  ULIInvisiblePlayerView.h
+//  ObjectCanvas
+//
+//  Created by Uli Kusterer on 2013-12-28.
+//  Copyright (c) 2013 Uli Kusterer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <AVFoundation/AVFoundation.h>
+
+
+@interface ULIInvisiblePlayerView : NSView
+
+@property (retain,nonatomic) AVPlayer* player;
+
+@end
---
+++ ULIInvisiblePlayerView.m
@@ -0,0 +1,51 @@
+//
+//  ULIInvisiblePlayerView.m
+//  ObjectCanvas
+//
+//  Created by Uli Kusterer on 2013-12-28.
+//  Copyright (c) 2013 Uli Kusterer. All rights reserved.
+//
+
+#import "ULIInvisiblePlayerView.h"
+
+
+@interface ULIInvisiblePlayerView ()
+
+@property (strong)	AVPlayerLayer	*	playerLayer;
+
+@end
+
+
+@implementation ULIInvisiblePlayerView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self)
+	{
+        self.wantsLayer = YES;
+		self.layer = [CALayer layer];
+    }
+    return self;
+}
+
+
+-(void)	setPlayer: (AVPlayer*)inPlayer
+{
+	if( !self.playerLayer )
+	{
+		self.playerLayer = [AVPlayerLayer playerLayerWithPlayer: inPlayer];
+		[self.playerLayer setFrame: self.layer.bounds];
+		[self.layer addSublayer: self.playerLayer];
+	}
+	else
+		self.playerLayer.player = inPlayer;
+}
+
+
+-(AVPlayer*)	player
+{
+	return self.playerLayer.player;
+}
+
+@end

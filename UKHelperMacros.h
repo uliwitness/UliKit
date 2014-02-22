1572f3a7b803552bdca6745823617c6fd2a31420
Uli Kusterer <ulivc@zathras.de>
2014-02-22 09:03:12 +0100

    Added a KEYPATH macro and more comments.

--- UKHelperMacros.h
+++ UKHelperMacros.h
@@ -2,7 +2,7 @@
 //  UKHelperMacros.h
 //
 //  Created by Uli Kusterer on 09.08.07.
-//  Copyright 2007 Uli Kusterer.
+//  Copyright 2007-2014 Uli Kusterer.
 //
 //	This software is provided 'as-is', without any express or implied
 //	warranty. In no event will the authors be held liable for any damages
@@ -45,6 +45,8 @@
 #endif	// UKLog
 
 
+// A pointer value that is guaranteed to cause a crash when a message is sent to it.
+//	Handy for assigning to instance variables in dealloc to catch talking to released objects.
 #if __LP64__
 #define	UKInvalidPointer		((id)0x5555555555555555)
 #else
@@ -68,35 +70,50 @@
 
 #define CREATE_AUTORELEASE_POOL(pool)		NSAutoreleasePool*	(pool) = [[NSAutoreleasePool alloc] init]
 
-#ifndef ASSIGN	// SenTest declares its own macro
+#ifndef ASSIGN	// SenTest declares its own macro of this name.
+// ASSIGN(ivar,val)
+//	Releases the current value in ivar and assigns val to it, retaining it. Doesn't release
+//	ivar's value until the new value has been assigned.
 #define ASSIGN(targ,newval)					do {\
 												NSObject* __UKHELPERMACRO_OLDTARG = (NSObject*)(targ);\
 												(targ) = [(newval) retain];\
 												[__UKHELPERMACRO_OLDTARG release];\
 											} while(0)
 #endif // !defined(ASSIGN)
+
+// ASSIGNMUTABLECOPY(ivar,val)
+//	Releases the current value in ivar and assigns [val mutableCopy] to it. Doesn't release
+//	ivar's value until the new value has been assigned.
 #define ASSIGNMUTABLECOPY(targ,newval)		do {\
 												NSObject* __UKHELPERMACRO_OLDTARG = (NSObject*)(targ);\
 												(targ) = [(newval) mutableCopy];\
 												[__UKHELPERMACRO_OLDTARG release];\
 											} while(0)
+// ASSIGNCOPY(ivar,val)
+//	Releases the current value in ivar and assigns [val copy] to it. Doesn't release
+//	ivar's value until the new value has been assigned.
 #define ASSIGNCOPY(targ,newval)				do {\
 												NSObject* __UKHELPERMACRO_OLDTARG = (NSObject*)(targ);\
 												(targ) = [(newval) copy];\
 												[__UKHELPERMACRO_OLDTARG release];\
 											} while(0)
 
+// DESTROY(ivar)
+//	Releases the variable 'ivar' and sets its value to NIL even *before* the release.
 #define DESTROY(targ)						do {\
 												NSObject* __UKHELPERMACRO_OLDTARG = (NSObject*)(targ);\
 												(targ) = nil;\
 												[__UKHELPERMACRO_OLDTARG release];\
 											} while(0)
+// DESTROY_DEALLOC(ivar)
+//	Releases the variable 'ivar' and sets its value to UKInvalidPointer even *before* the release.
 #define DESTROY_DEALLOC(targ)				do {\
 												NSObject* __UKHELPERMACRO_OLDTARG = (NSObject*)(targ);\
 												(targ) = UKInvalidPointer;\
 												[__UKHELPERMACRO_OLDTARG release];\
 											} while(0)
 
+// PROPERTY(propertyName) --> @"propertyName"
 //	The following macro is for specifying property (ivar) names to KVC or KVO methods.
 //	These methods generally take strings, but strings don't get checked for typos
 //	by the compiler. If you write PROPERTY(fremen) instead of PROPERTY(frame),
@@ -104,13 +121,27 @@
 //	'fremen', and thus point out the typo. For this to work, you need to make
 //	sure the warning -Wunknown-selector is on.
 //
-//	The code that checks here is (theoretically) slower than just using a string
-//	literal, so what we do is we only do the checking in debug builds. In
-//	release builds, we use the identifier-stringification-operator "#" to turn
-//	the given property name into an ObjC string literal.
+//	Thanks to the NO ? trick goes to Rob Rix. This means that the NSStringFromSelector()
+//	is compiled (and causes errors with invalid selectors) but is never used at runtime
+//	(so performance of debug and non-debug builds stays identical).
 
 #if DEBUG
-#define PROPERTY(propName)	NSStringFromSelector(@selector(propName))
+#define PROPERTY(propName)	(YES ? @ # propName : NSStringFromSelector(@selector(propName)))
 #else
 #define PROPERTY(propName)	@#propName
+#endif
+
+
+// KEYPATH(obj,key.path.on.object) --> @"key.path.on.object"
+//	The following macro is like PROPERTY, but for entire key paths. Thanks to
+//	Rob Rix for donating this. The else case essentially returns NIL cast to NSString*,
+//	but in the process of determining the type happens to validate our path as a side effect.
+//	Since the else case is unreachable, this has the same runtime effect as specifying a string
+//	constant, but it is type-checked.
+//	You only pass in 'obj' to allow determining the type. It doesn't actually call anything.
+
+#if DEBUG
+#define KEYPATH(OBJ, PATH) (YES ? @ # PATH : (__typeof__(^ NSString * { (void)((__typeof__(OBJ))nil).PATH; return nil; }())) nil)
+#else
+#define KEYPATH(OBJ, PATH) (@ # PATH)
 #endif

baee60cc9eaa653de9dbf6e73beb481488175827
C.W. Betts <computers57@hotmail.com>
2019-03-15 01:14:50 -0600

    Update UKXattrMetadataStore.h Add more Swift-friendly methods.

--- UKXattrMetadataStore.h
+++ UKXattrMetadataStore.h
@@ -1,28 +1,10 @@
 //
 //  UKXattrMetadataStore.h
 //  BubbleBrowser
+//	LICENSE: MIT License
 //
 //  Created by Uli Kusterer on 12.03.06.
 //  Copyright 2006 Uli Kusterer. All rights reserved.
-//
-//	This software is provided 'as-is', without any express or implied
-//	warranty. In no event will the authors be held liable for any damages
-//	arising from the use of this software.
-//
-//	Permission is granted to anyone to use this software for any purpose,
-//	including commercial applications, and to alter it and redistribute it
-//	freely, subject to the following restrictions:
-//
-//	   1. The origin of this software must not be misrepresented; you must not
-//	   claim that you wrote the original software. If you use this software
-//	   in a product, an acknowledgment in the product documentation would be
-//	   appreciated but is not required.
-//
-//	   2. Altered source versions must be plainly marked as such, and must not be
-//	   misrepresented as being the original software.
-//
-//	   3. This notice may not be removed or altered from any source
-//	   distribution.
 //
 
 // -----------------------------------------------------------------------------
@@ -31,7 +13,10 @@
 
 #import <Cocoa/Cocoa.h>
 
-/*
+/*!
+ @header UKXattrMetadataStore.h
+ 
+ @discussion
 	This is a wrapper around The Mac OS X 10.4 and later xattr API that lets
 	you attach arbitrary metadata to a file. Currently it allows querying and
 	changing the attributes of a file, as well as retrieving a list of attribute
@@ -49,31 +34,226 @@
 //	Class declaration:
 // -----------------------------------------------------------------------------
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface UKXattrMetadataStore : NSObject
-{
-	
-}
 
-+(NSArray*)		allKeysAtPath: (NSString*)path traverseLink:(BOOL)travLnk;
+/*!
+ *	@method		allKeysAtPath:traverseLink:
+ *	@param		path
+ *				The file to get xattr names from.
+ *	@param		travLnk
+ *				If <code>YES</code>, follows symlinks.
+ *	@return		An \c NSArray of <code>NSString</code>s, or \c nil on failure.
+ *	@discussion	Returns an \c NSArray of <code>NSString</code>s containing all xattr names currently set
+ *				for the file at the specified path.
+ */
++(nullable NSArray<NSString*>*) allKeysAtPath:(NSString*)path traverseLink:(BOOL)travLnk;
 
-// Store UTF8 strings:
-+(void)				setString: (NSString*)str forKey: (NSString*)key
-						atPath: (NSString*)path traverseLink:(BOOL)travLnk;
-+(id)				stringForKey: (NSString*)key atPath: (NSString*)path
-						traverseLink:(BOOL)travLnk;
+#pragma mark Store UTF8 strings:
+/*!
+ *	@method		setString:forKey:atPath:traverseLink:
+ *	@brief		Set the xattr with name \c key to the UTF8 representation of <code>str</code>.
+ *	@param		str
+ *				The string to set.
+ *	@param		key
+ *				the key to set \c str to.
+ *	@param		path
+ *				The file whose xattr you want to set.
+ *	@param		travLnk
+ *				If <code>YES</code>, follows symlinks.
+ *	@discussion	Set the xattr with name key to an XML property list representation of
+ *				the specified object (or object graph).
+ *	@deprecated	This method throws an Obj-C exception. No other error information is provided, not even if it was successful.
+ */
++(void) setString:(NSString*)str forKey:(NSString*)key
+		   atPath:(NSString*)path traverseLink:(BOOL)travLnk DEPRECATED_ATTRIBUTE NS_SWIFT_UNAVAILABLE("Use 'setString(_:forKey:atPath:traverseLink:) throws' instead");
 
-// Store raw data:
-+(void)				setData: (NSData*)data forKey: (NSString*)key
-						atPath: (NSString*)path traverseLink:(BOOL)travLnk;
-+(NSMutableData*)	dataForKey: (NSString*)key atPath: (NSString*)path
-						traverseLink:(BOOL)travLnk;
+/*!
+ *	@method		setString:forKey:atPath:traverseLink:error:
+ *	@brief		Set the xattr with name \c key to the UTF8 representation of <code>str</code>.
+ *	@param		str
+ *				The string to set.
+ *	@param		key
+ *				the key to set \c str to.
+ *	@param		path
+ *				The file whose xattr you want to set.
+ *	@param		travLnk
+ *				If <code>YES</code>, follows symlinks.
+ *	@param		outError
+ *				If the method does not complete successfully, upon return 
+ *				contains an \c NSError object that describes the problem.
+ *	@return		\c YES on success, \c NO on failure.
+ *	@discussion	Set the xattr with name \c key to the UTF8 representation of <code>str</code>.
+ */
++(BOOL) setString:(NSString*)str forKey:(NSString*)key
+		   atPath:(NSString*)path traverseLink:(BOOL)travLnk error:(NSError**)outError;
 
-// Store objects: (Only can get/set plist-type objects for now)â
-+(void)				setObject: (id)obj forKey: (NSString*)key atPath: (NSString*)path
-						traverseLink:(BOOL)travLnk;
-+(id)				objectForKey: (NSString*)key atPath: (NSString*)path
-						traverseLink:(BOOL)travLnk;
+/*!
+ *	@method		stringForKey:atPath:traverseLink:
+ *	@brief		Get the xattr with name \c key as a UTF8 string.
+ *	@param		key
+ *				the key to set \c str to.
+ *	@param		path
+ *				The file whose xattr you want to get.
+ *	@param		travLnk
+ *				If <code>YES</code>, follows symlinks.
+ *	@return		an \c NSString on succes, or \c nil on failure.
+ *	@discussion	Get the xattr with name \c key as a UTF8 string.
+ *	@deprecated	This method has no error handling.
+ */
++(nullable NSString*) stringForKey:(NSString*)key atPath:(NSString*)path
+					  traverseLink:(BOOL)travLnk DEPRECATED_ATTRIBUTE NS_SWIFT_UNAVAILABLE("Use 'string(forKey:atPath:traverseLink:) throws' instead");
+
+/*!
+ *	@method		stringForKey:atPath:traverseLink:error:
+ *	@brief		Get the xattr with name \c key as a UTF8 string.
+ *	@param		key
+ *				the key to set \c str to.
+ *	@param		path
+ *				The file whose xattr you want to get.
+ *	@param		travLnk
+ *				If <code>YES</code>, follows symlinks.
+ *	@param		error
+ *				If the method does not complete successfully, upon return
+ *				contains an \c NSError object that describes the problem.
+ *	@return		an \c NSString on succes, or \c nil on failure.
+ *	@discussion	Get the xattr with name \c key as a UTF-8 string.
+ */
++(nullable NSString*) stringForKey:(NSString*)key atPath:(NSString*)path
+					  traverseLink:(BOOL)travLnk error:(NSError**)error;
+
+#pragma mark Store raw data:
+/*!
+ *	@method		setData:forKey:atPath:traverseLink:
+ *	@brief		Set the xattr with name \c key to the raw data in <code>data</code>.
+ *	@param		data
+ *				The data to set.
+ *	@param		key
+ *				the key to set \c data to.
+ *	@param		path
+ *				The file whose xattr you want to set.
+ *	@param		travLnk
+ *				If <code>YES</code>, follows symlinks.
+ *	@discussion	Set the xattr with name key to an XML property list representation of
+ *				the specified object (or object graph).
+ *	@deprecated	This method has no way of indicating success or failure.
+ */
++(void) setData:(NSData*)data forKey:(NSString*)key
+		 atPath:(NSString*)path traverseLink:(BOOL)travLnk DEPRECATED_ATTRIBUTE NS_SWIFT_UNAVAILABLE("Use 'setData(_:forKey:atPath:traverseLink:) throws' instead");
+/*!
+ *	@method		setData:forKey:atPath:traverseLink:error:
+ *	@brief		Set the xattr with name \c key to the raw data in <code>data</code>.
+ *	@param		data
+ *				The data to set.
+ *	@param		key
+ *				the key to set \c data to.
+ *	@param		path
+ *				The file whose xattr you want to set.
+ *	@param		travLnk
+ *				If <code>YES</code>, follows symlinks.
+ *	@param		error
+ *				If the method does not complete successfully, upon return
+ *				contains an \c NSError object that describes the problem.
+ *	@return		\c YES on success, \c NO on failure.
+ *	@discussion	Set the xattr with name \c key to the raw data in <code>data</code>.
+ */
++(BOOL) setData:(NSData*)data forKey:(NSString*)key
+		 atPath:(NSString*)path traverseLink:(BOOL)travLnk error:(NSError**)error;
+
+/*!
+ *	@method		dataForKey:atPath:traverseLink:
+ *	@brief		Get the xattr with name \c key as raw data.
+ *	@param		key
+ *				the key to set \c str to.
+ *	@param		path
+ *				The file whose xattr you want to get.
+ *	@param		travLnk
+ *				If <code>YES</code>, follows symlinks.
+ *	@return		an \c NSData containing the contents of \c key on succes, or \c nil on failure
+ *	@discussion	Get the xattr with name \c key as a UTF8 string
+ *	@deprecated	This method throws an Obj-C exception. No other error information is provoded on failure.
+ */
++(nullable NSData*) dataForKey:(NSString*)key atPath:(NSString*)path
+				  traverseLink:(BOOL)travLnk DEPRECATED_ATTRIBUTE NS_SWIFT_UNAVAILABLE("Use 'data(forKey:atPath:traverseLink:) throws' instead");
+/*!
+ *	@method		dataForKey:atPath:traverseLink:error:
+ *	@brief		Get the xattr with name \c key as raw data.
+ *	@param		key
+ *				the key to set \c str to.
+ *	@param		path
+ *				The file whose xattr you want to get.
+ *	@param		travLnk
+ *				If <code>YES</code>, follows symlinks.
+ *	@param		error
+ *				If the method does not complete successfully, upon return
+ *				contains an \c NSError object that describes the problem.
+ *	@return		an \c NSData containing the contents of \c key on succes, or \c nil on failure
+ *	@discussion	Get the xattr with name \c key as a UTF8 string
+ */
++(nullable NSData*) dataForKey:(NSString*)key atPath:(NSString*)path
+				  traverseLink:(BOOL)travLnk error:(NSError**)error;
+
+#pragma mark Store objects: (Only can get/set plist-type objects for now)â€š
+/*!
+ *	@method		setObject:forKey:atPath:traverseLink:
+ *	@param		obj
+ *				The property list object to set.
+ *	@param		key
+ *				the key to set \c obj to.
+ *	@param		path
+ *				The file whose xattr you want to set.
+ *	@param		travLnk
+ *				If <code>YES</code>, follows symlinks.
+ *	@discussion	Set the xattr with name key to an XML property list representation of
+ *				the specified object (or object graph).
+ *	@deprecated	This method throws an Obj-C exception. No other error information is provided,
+ *				not even if it was successful.
+ */
++(void) setObject:(id)obj forKey:(NSString*)key atPath:(NSString*)path
+	 traverseLink:(BOOL)travLnk DEPRECATED_ATTRIBUTE NS_SWIFT_UNAVAILABLE("Use 'setObject(_:forKey:atPath:traverseLink:) throws' instead");
+
+/*!
+ *	@method		setObject:forKey:atPath:traverseLink:error:
+ *	@param		obj
+ *				The Property List object to set.
+ *	@param		key
+ *				the key to set \obj to.
+ *	@param		path
+ *				The file whose xattr you want to set.
+ *	@param		travLnk
+ *				If <code>YES</code>, follows symlinks.
+ *	@param		error
+ *				If the method does not complete successfully, upon return
+ *				contains an \c NSError object that describes the problem.
+ *	@return		\c YES on success, \c NO on failure.
+ *	@discussion	Set the xattr with name \c key to an XML property list representation of
+ *				the specified object (or object graph).
+ */
++(BOOL) setObject:(id)obj forKey:(NSString*)key atPath:(NSString*)path
+	 traverseLink:(BOOL)travLnk error:(NSError**)error;
+
+/*!
+ *	@method		objectForKey:atPath:traverseLink:error:
+ *	@brief		Get the xattr with name \c key as a property list
+ *	@param		key
+ *				the key to get the Property List object from.
+ *	@param		path
+ *				The file whose xattr you want to get.
+ *	@param		travLnk
+ *				If <code>YES</code>, follows symlinks.
+ *	@param		outError
+ *				If the method does not complete successfully, upon return
+ *				contains an \c NSError object that describes the problem.
+ *	@return		a Property List object from contents of \c key on succes, or \c nil on failure
+ *	@discussion	Get the xattr with name \c key as a property list object (<code>NSString</code>, <code>NSArray</code>, etc...)<br>
+ *				The data has to be stored as an XML property list.
+ */
++(nullable id) objectForKey:(NSString*)key atPath:(NSString*)path
+			   traverseLink:(BOOL)travLnk error:(NSError**)outError;
 
 @end
+
+NS_ASSUME_NONNULL_END
 
 #endif /*MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_4*/
--- UKXattrMetadataStore.m
+++ UKXattrMetadataStore.m
@@ -1,28 +1,10 @@
 //
 //  UKXattrMetadataStore.m
 //  BubbleBrowser
+//	LICENSE: MIT License
 //
 //  Created by Uli Kusterer on 12.03.06.
-//  Copyright 2006 Uli Kusterer.
-//	
-//	This software is provided 'as-is', without any express or implied
-//	warranty. In no event will the authors be held liable for any damages
-//	arising from the use of this software.
-//
-//	Permission is granted to anyone to use this software for any purpose,
-//	including commercial applications, and to alter it and redistribute it
-//	freely, subject to the following restrictions:
-//
-//	   1. The origin of this software must not be misrepresented; you must not
-//	   claim that you wrote the original software. If you use this software
-//	   in a product, an acknowledgment in the product documentation would be
-//	   appreciated but is not required.
-//
-//	   2. Altered source versions must be plainly marked as such, and must not be
-//	   misrepresented as being the original software.
-//
-//	   3. This notice may not be removed or altered from any source
-//	   distribution.
+//  Copyright 2006 Uli Kusterer. All rights reserved.
 //
 
 #if MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_4
@@ -31,7 +13,7 @@
 // -----------------------------------------------------------------------------
 
 #import "UKXattrMetadataStore.h"
-#import <sys/xattr.h>
+#include <sys/xattr.h>
 
 
 @implementation UKXattrMetadataStore
@@ -43,31 +25,25 @@
 //		If travLnk == YES, it follows symlinks.
 // -----------------------------------------------------------------------------
 
-+(NSArray*)		allKeysAtPath: (NSString*)path traverseLink:(BOOL)travLnk
++(NSArray*) allKeysAtPath: (NSString*)path traverseLink:(BOOL)travLnk
 {
-	NSMutableArray*	allKeys = [NSMutableArray array];
+	NSMutableArray<NSString*>*	allKeys = [NSMutableArray array];
 	size_t dataSize = listxattr( [path fileSystemRepresentation],
 								NULL, ULONG_MAX,
 								(travLnk ? 0 : XATTR_NOFOLLOW) );
 	if( dataSize == ULONG_MAX )
-		return allKeys;	// Empty list.
+		return nil;	// Empty list.
 	NSMutableData*	listBuffer = [NSMutableData dataWithLength: dataSize];
 	dataSize = listxattr( [path fileSystemRepresentation],
 							[listBuffer mutableBytes], [listBuffer length],
 							(travLnk ? 0 : XATTR_NOFOLLOW) );
-	char*	nameStart = [listBuffer mutableBytes];
-	int x;
-	for( x = 0; x < dataSize; x++ )
-	{
-		if( ((char*)[listBuffer mutableBytes])[x] == 0 )	// End of string.
-		{
-			NSString*	str = [NSString stringWithUTF8String: nameStart];
-			nameStart = [listBuffer mutableBytes] +x +1;
-			[allKeys addObject: str];
-		}
+	NSString *allStrKeys = [[NSString alloc] initWithData:listBuffer encoding:NSUTF8StringEncoding];
+	[allKeys setArray:[allStrKeys componentsSeparatedByString:@"\0"]];
+	if (allKeys.lastObject.length == 0) {
+		[allKeys removeLastObject];
 	}
 	
-	return allKeys;
+	return [allKeys copy];
 }
 
 
@@ -78,14 +54,24 @@
 //		If travLnk == YES, it follows symlinks.
 // -----------------------------------------------------------------------------
 
-+(void)				setData: (NSData*)data forKey: (NSString*)key atPath: (NSString*)path traverseLink:(BOOL)travLnk
++(void) setData: (NSData*)data forKey: (NSString*)key atPath: (NSString*)path traverseLink:(BOOL)travLnk
 {
-	setxattr( [path fileSystemRepresentation], [key UTF8String],
-				[data bytes], [data length],
-				0, (travLnk ? 0 : XATTR_NOFOLLOW) );
+	[self setData:data forKey:key atPath:path traverseLink:travLnk error:NULL];
 }
 
-
++(BOOL) setData: (NSData*)data forKey: (NSString*)key atPath: (NSString*)path traverseLink:(BOOL)travLnk error:(NSError**)error
+{
+	int iErr = setxattr([path fileSystemRepresentation], [key UTF8String],
+				[data bytes], [data length],
+				0, (travLnk ? 0 : XATTR_NOFOLLOW) );
+	if (iErr == -1) {
+		if (error) {
+			*error = [NSError errorWithDomain:NSPOSIXErrorDomain code:errno userInfo:@{NSFilePathErrorKey: path}];
+		}
+		return NO;
+	}
+	return YES;
+}
 // -----------------------------------------------------------------------------
 //	setObject:forKey:atPath:traverseLink:
 //		Set the xattr with name key to an XML property list representation of
@@ -103,11 +89,24 @@
 								errorDescription: &errMsg];
 	if( errMsg )
 	{
-		[errMsg autorelease];
 		[NSException raise: @"UKXattrMetastoreCantSerialize" format: @"%@", errMsg];
 	}
 	else
-		[[self class] setData: plistData forKey: key atPath: path traverseLink: travLnk];
+		[self setData: plistData forKey: key atPath: path traverseLink: travLnk error: NULL];
+}
+
++(BOOL)	setObject: (id)obj forKey: (NSString*)key atPath: (NSString*)path traverseLink:(BOOL)travLnk error:(NSError**)error
+{
+	// Serialize our objects into a property list XML string:
+	NSData *plistData = [NSPropertyListSerialization dataWithPropertyList:obj format:NSPropertyListXMLFormat_v1_0 options:0 error:error];
+
+	if( !plistData )
+	{
+		//NSPropertyListSerialization should have filled out the error.
+		return NO;
+	}
+	else
+		return [self setData: plistData forKey: key atPath: path traverseLink: travLnk error: error];
 }
 
 
@@ -126,9 +125,25 @@
 	if( !data )
 		[NSException raise: NSCharacterConversionException format: @"Couldn't convert string to UTF8 for xattr storage."];
 	
-	[[self class] setData: data forKey: key atPath: path traverseLink: travLnk];
+	[[self class] setData: data forKey: key atPath: path traverseLink: travLnk error: nil];
 }
 
++(BOOL)	setString: (NSString*)str forKey: (NSString*)key atPath: (NSString*)path traverseLink:(BOOL)travLnk error:(NSError * _Nullable __autoreleasing * _Nullable)outError
+{
+	NSData *data = [str dataUsingEncoding: NSUTF8StringEncoding];
+	
+	if (!data) {
+		if (outError) {
+			*outError = [NSError errorWithDomain:NSCocoaErrorDomain code:NSFileWriteInapplicableStringEncodingError userInfo:
+						 @{NSLocalizedDescriptionKey: @"Couldn't convert string to UTF8 for xattr storage.",
+						   NSStringEncodingErrorKey: @(NSUTF8StringEncoding),
+						   NSFilePathErrorKey: path}];
+		}
+		return NO;
+	}
+	
+	return [self setData: data forKey: key atPath: path traverseLink: travLnk error: outError];
+}
 
 // -----------------------------------------------------------------------------
 //	dataForKey:atPath:traverseLink:
@@ -137,17 +152,33 @@
 //		If travLnk == YES, it follows symlinks.
 // -----------------------------------------------------------------------------
 
-+(NSMutableData*)	dataForKey: (NSString*)key atPath: (NSString*)path traverseLink:(BOOL)travLnk
++(NSData*) dataForKey: (NSString*)key atPath: (NSString*)path traverseLink:(BOOL)travLnk
+{
+	return [self dataForKey:key atPath:path traverseLink:travLnk error:NULL];
+}
+
++(NSData*) dataForKey: (NSString*)key atPath: (NSString*)path traverseLink:(BOOL)travLnk error:(NSError * _Nullable __autoreleasing * _Nullable)error
 {
 	size_t		dataSize = getxattr( [path fileSystemRepresentation], [key UTF8String],
-										NULL, ULONG_MAX, 0, (travLnk ? 0 : XATTR_NOFOLLOW) );
-	if( dataSize == ULONG_MAX )
+									NULL, ULONG_MAX, 0, (travLnk ? 0 : XATTR_NOFOLLOW) );
+	if( dataSize == ULONG_MAX ) {
+		if (error) {
+			*error = [NSError errorWithDomain:NSPOSIXErrorDomain code:errno userInfo:@{NSFilePathErrorKey: path}];
+		}
 		return nil;
-	NSMutableData*	data = [NSMutableData dataWithLength: dataSize];
-	getxattr( [path fileSystemRepresentation], [key UTF8String],
+	}
+	NSMutableData*	data = [[NSMutableData alloc] initWithLength: dataSize];
+	dataSize = getxattr( [path fileSystemRepresentation], [key UTF8String],
 				[data mutableBytes], [data length], 0, (travLnk ? 0 : XATTR_NOFOLLOW) );
 	
-	return data;
+	if (dataSize == -1) {
+		if (error) {
+			*error = [NSError errorWithDomain:NSPOSIXErrorDomain code:errno userInfo:@{NSFilePathErrorKey: path}];
+		}
+		return nil;
+	}
+	
+	return [data copy];
 }
 
 
@@ -159,21 +190,31 @@
 //		If travLnk == YES, it follows symlinks.
 // -----------------------------------------------------------------------------
 
-+(id)	objectForKey: (NSString*)key atPath: (NSString*)path traverseLink:(BOOL)travLnk
++(id) objectForKey: (NSString*)key atPath: (NSString*)path traverseLink:(BOOL)travLnk
 {
-	NSString*				errMsg = nil;
-	NSMutableData*			data = [[self class] dataForKey: key atPath: path traverseLink: travLnk];
-	if( !data )
+	NSError		*err = nil;
+	id obj = [self objectForKey:key atPath:path traverseLink:travLnk error:&err];
+	if (!obj) {
+		[NSException raise:@"UKXattrMetastoreCantUnserialize" format: @"%@", err];
+	}
+	
+	return obj;
+}
+
++(nullable id) objectForKey: (NSString*)key atPath: (NSString*)path
+			   traverseLink: (BOOL)travLnk error: (NSError**)outError
+{
+	NSData *data = [[self class] dataForKey: key atPath: path traverseLink: travLnk error: outError];
+	if (!data) {
+		//The dataForKey:... method should have filled out the error variable.
 		return nil;
+	}
 	NSPropertyListFormat	outFormat = NSPropertyListXMLFormat_v1_0;
-	id obj = [NSPropertyListSerialization propertyListFromData: data
-					mutabilityOption: NSPropertyListImmutable
-					format: &outFormat
-					errorDescription: &errMsg];
-	if( errMsg )
-	{
-		[errMsg autorelease];
-		[NSException raise: @"UKXattrMetastoreCantUnserialize" format: @"%@", errMsg];
+
+	id obj = [NSPropertyListSerialization propertyListWithData:data options:NSPropertyListImmutable format:&outFormat error:outError];
+	if (!obj) {
+		//The propertyListWithData:... method should have filled out the error variable.
+		return nil;
 	}
 	
 	return obj;
@@ -188,14 +229,38 @@
 //		If travLnk == YES, it follows symlinks.
 // -----------------------------------------------------------------------------
 
-+(id)	stringForKey: (NSString*)key atPath: (NSString*)path traverseLink:(BOOL)travLnk
++(NSString*) stringForKey: (NSString*)key atPath: (NSString*)path traverseLink:(BOOL)travLnk
 {
-	NSMutableData*			data = [[self class] dataForKey: key atPath: path traverseLink: travLnk];
-	
-	if( !data )
+	NSData *data = [[self class] dataForKey: key atPath: path traverseLink: travLnk error: nil];
+	if (!data) {
 		return nil;
+	}
 	
-	return [[[NSString alloc] initWithData: data encoding: NSUTF8StringEncoding] autorelease];
+	return [[NSString alloc] initWithData: data encoding: NSUTF8StringEncoding];
+}
+
++(NSString*) stringForKey: (NSString*)key atPath: (NSString*)path traverseLink:(BOOL)travLnk error:(NSError * _Nullable __autoreleasing * _Nullable)error
+{
+	NSData *data = [[self class] dataForKey: key atPath: path traverseLink: travLnk error: error];
+	
+	if (!data) {
+		//The dataForKey:... method should have filled out the error variable.
+		return nil;
+	}
+	
+	NSString *toRet = [[NSString alloc] initWithData: data encoding: NSUTF8StringEncoding];
+	
+	if (!toRet) {
+		if (error) {
+			*error = [NSError errorWithDomain: NSCocoaErrorDomain
+										 code: NSFileReadInapplicableStringEncodingError
+									 userInfo:
+					  @{NSStringEncodingErrorKey: @(NSUTF8StringEncoding),
+						NSFilePathErrorKey: path}];
+		}
+	}
+	
+	return toRet;
 }
 
 

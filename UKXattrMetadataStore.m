93961243fb472d00a0c206ad33d1dcd925e6c2f2
Uli Kusterer <ulivc@zathras.de>
2019-03-16 12:05:42 +0100

    Mostly cosmetic changes. Better Swift names, better ObjC names for the new versions of the Plist methods, deprecation messages, got rid of old 10.4 guard (ARC requires at least 10.6, after all), formatting.

--- UKXattrMetadataStore.m
+++ UKXattrMetadataStore.m
@@ -25,27 +25,26 @@
 //	   distribution.
 //
 
-#if MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_4
-// -----------------------------------------------------------------------------
-//	Headers:
-// -----------------------------------------------------------------------------
-
 #if !__has_feature(objc_arc)
 #error this file needs to be compiled with Automatic Reference Counting (ARC)
 #endif
+
+// -----------------------------------------------------------------------------
+//	Headers:
+// -----------------------------------------------------------------------------
 
 #import "UKXattrMetadataStore.h"
 #include <sys/xattr.h>
 
 
-@implementation UKXattrMetadataStore
+// -----------------------------------------------------------------------------
+//	Constants:
+// -----------------------------------------------------------------------------
 
-// -----------------------------------------------------------------------------
-//	allKeysAtPath:traverseLink:
-//		Return an NSArray of NSStrings containing all xattr names currently set
-//		for the file at the specified path.
-//		If travLnk == YES, it follows symlinks.
-// -----------------------------------------------------------------------------
+const NSInteger ULIMaxXAttrKeyLength = 127;
+
+
+@implementation UKXattrMetadataStore
 
 +(NSArray*) allKeysAtPath: (NSString*)path traverseLink:(BOOL)travLnk
 {
@@ -53,14 +52,17 @@
 	size_t dataSize = listxattr( [path fileSystemRepresentation],
 								NULL, ULONG_MAX,
 								(travLnk ? 0 : XATTR_NOFOLLOW) );
-	if( dataSize == ULONG_MAX )
+	if (dataSize == ULONG_MAX) {
 		return allKeys;	// Empty list.
+	}
+	
 	NSMutableData*	listBuffer = [NSMutableData dataWithLength: dataSize];
 	dataSize = listxattr( [path fileSystemRepresentation],
 							[listBuffer mutableBytes], [listBuffer length],
 							(travLnk ? 0 : XATTR_NOFOLLOW) );
-	NSString *allStrKeys = [[NSString alloc] initWithData:listBuffer encoding:NSUTF8StringEncoding];
-	[allKeys setArray:[allStrKeys componentsSeparatedByString:@"\0"]];
+	
+	NSString *allStrKeys = [[NSString alloc] initWithData: listBuffer encoding: NSUTF8StringEncoding];
+	[allKeys setArray: [allStrKeys componentsSeparatedByString:@"\0"]];
 	if (allKeys.lastObject.length == 0) {
 		[allKeys removeLastObject];
 	}
@@ -69,38 +71,28 @@
 }
 
 
-// -----------------------------------------------------------------------------
-//	setData:forKey:atPath:traverseLink:
-//		Set the xattr with name key to a block of raw binary data.
-//		path is the file whose xattr you want to set.
-//		If travLnk == YES, it follows symlinks.
-// -----------------------------------------------------------------------------
-
 +(void) setData: (NSData*)data forKey: (NSString*)key atPath: (NSString*)path traverseLink:(BOOL)travLnk
 {
-	[self setData:data forKey:key atPath:path traverseLink:travLnk error:NULL];
+	[self setData: data forKey: key atPath: path traverseLink: travLnk error: NULL];
 }
 
-+(BOOL) setData: (NSData*)data forKey: (NSString*)key atPath: (NSString*)path traverseLink:(BOOL)travLnk error:(NSError**)error
+
++(BOOL) setData: (NSData*)data forKey: (NSString*)key atPath: (NSString*)path traverseLink:(BOOL)travLnk error:(NSError * _Nullable __autoreleasing * _Nullable)error
 {
+	NSAssert(key.length <= ULIMaxXAttrKeyLength, @"Key length limit exceeded.");
+
 	int iErr = setxattr([path fileSystemRepresentation], [key UTF8String],
 				[data bytes], [data length],
 				0, (travLnk ? 0 : XATTR_NOFOLLOW) );
 	if (iErr == -1) {
 		if (error) {
-			*error = [NSError errorWithDomain:NSPOSIXErrorDomain code:errno userInfo:@{NSFilePathErrorKey: path}];
+			*error = [NSError errorWithDomain: NSPOSIXErrorDomain code: errno userInfo: @{NSFilePathErrorKey: path}];
 		}
 		return NO;
 	}
 	return YES;
 }
-// -----------------------------------------------------------------------------
-//	setObject:forKey:atPath:traverseLink:
-//		Set the xattr with name key to an XML property list representation of
-//		the specified object (or object graph).
-//		path is the file whose xattr you want to set.
-//		If travLnk == YES, it follows symlinks.
-// -----------------------------------------------------------------------------
+
 
 +(void)	setObject: (id)obj forKey: (NSString*)key atPath: (NSString*)path traverseLink:(BOOL)travLnk
 {
@@ -109,43 +101,35 @@
 	NSData*		plistData = [NSPropertyListSerialization dataFromPropertyList: obj
 								format: NSPropertyListXMLFormat_v1_0
 								errorDescription: &errMsg];
-	if( errMsg )
-	{
+	if (errMsg) {
 		[NSException raise: @"UKXattrMetastoreCantSerialize" format: @"%@", errMsg];
-	}
-	else
+	} else {
 		[self setData: plistData forKey: key atPath: path traverseLink: travLnk error: NULL];
+	}
 }
 
-+(BOOL)	setObject: (id)obj forKey: (NSString*)key atPath: (NSString*)path traverseLink:(BOOL)travLnk error:(NSError**)error
+
++(BOOL)	setPlist: (id)obj asXMLForKey: (NSString*)key atPath: (NSString*)path traverseLink:(BOOL)travLnk error:(NSError * _Nullable __autoreleasing * _Nullable)error
 {
 	// Serialize our objects into a property list XML string:
 	NSData *plistData = [NSPropertyListSerialization dataWithPropertyList:obj format:NSPropertyListXMLFormat_v1_0 options:0 error:error];
 
-	if( !plistData )
-	{
+	if (!plistData) {
 		//NSPropertyListSerialization should have filled out the error.
 		return NO;
-	}
-	else
+	} else {
 		return [self setData: plistData forKey: key atPath: path traverseLink: travLnk error: error];
+	}
 }
 
-
-// -----------------------------------------------------------------------------
-//	setString:forKey:atPath:traverseLink:
-//		Set the xattr with name key to an XML property list representation of
-//		the specified object (or object graph).
-//		path is the file whose xattr you want to set.
-//		If travLnk == YES, it follows symlinks.
-// -----------------------------------------------------------------------------
 
 +(void)	setString: (NSString*)str forKey: (NSString*)key atPath: (NSString*)path traverseLink:(BOOL)travLnk
 {
 	NSData*		data = [str dataUsingEncoding: NSUTF8StringEncoding];
 	
-	if( !data )
+	if( !data ) {
 		[NSException raise: NSCharacterConversionException format: @"Couldn't convert string to UTF8 for xattr storage."];
+	}
 	
 	[[self class] setData: data forKey: key atPath: path traverseLink: travLnk error: nil];
 }
@@ -167,20 +151,17 @@
 	return [self setData: data forKey: key atPath: path traverseLink: travLnk error: outError];
 }
 
-// -----------------------------------------------------------------------------
-//	dataForKey:atPath:traverseLink:
-//		Retrieve the xattr with name key as a raw block of data.
-//		path is the file whose xattr you want to set.
-//		If travLnk == YES, it follows symlinks.
-// -----------------------------------------------------------------------------
 
-+(NSData*) dataForKey: (NSString*)key atPath: (NSString*)path traverseLink:(BOOL)travLnk
++(nullable NSData*) dataForKey: (NSString*)key atPath: (NSString*)path traverseLink:(BOOL)travLnk
 {
 	return [self dataForKey:key atPath:path traverseLink:travLnk error:NULL];
 }
 
-+(NSData*) dataForKey: (NSString*)key atPath: (NSString*)path traverseLink:(BOOL)travLnk error:(NSError * _Nullable __autoreleasing * _Nullable)error
+
++(nullable NSData*) dataForKey: (NSString*)key atPath: (NSString*)path traverseLink:(BOOL)travLnk error:(NSError * _Nullable __autoreleasing * _Nullable)error
 {
+	NSAssert(key.length <= ULIMaxXAttrKeyLength, @"Key length limit exceeded.");
+	
 	size_t		dataSize = getxattr( [path fileSystemRepresentation], [key UTF8String],
 									NULL, ULONG_MAX, 0, (travLnk ? 0 : XATTR_NOFOLLOW) );
 	if( dataSize == ULONG_MAX ) {
@@ -204,18 +185,12 @@
 }
 
 
-// -----------------------------------------------------------------------------
-//	objectForKey:atPath:traverseLink:
-//		Retrieve the xattr with name key, which is an XML property list
-//		and unserialize it back into an object or object graph.
-//		path is the file whose xattr you want to set.
-//		If travLnk == YES, it follows symlinks.
-// -----------------------------------------------------------------------------
-
-+(id) objectForKey: (NSString*)key atPath: (NSString*)path traverseLink:(BOOL)travLnk
++(nullable id) objectForKey: (NSString*)key atPath: (NSString*)path traverseLink:(BOOL)travLnk
 {
+	NSAssert(key.length <= ULIMaxXAttrKeyLength, @"Key length limit exceeded.");
+	
 	NSError		*err = nil;
-	id obj = [self objectForKey:key atPath:path traverseLink:travLnk error:&err];
+	id obj = [self plistForXMLInKey: key atPath: path traverseLink: travLnk error: &err];
 	if (!obj) {
 		[NSException raise:@"UKXattrMetastoreCantUnserialize" format: @"%@", err];
 	}
@@ -223,9 +198,11 @@
 	return obj;
 }
 
-+(nullable id) objectForKey: (NSString*)key atPath: (NSString*)path
-			   traverseLink: (BOOL)travLnk error: (NSError**)outError
++(nullable id) plistForXMLInKey: (NSString*)key atPath: (NSString*)path
+			   traverseLink: (BOOL)travLnk error: (NSError * _Nullable __autoreleasing * _Nullable)outError
 {
+	NSAssert(key.length <= ULIMaxXAttrKeyLength, @"Key length limit exceeded.");
+	
 	NSData *data = [[self class] dataForKey: key atPath: path traverseLink: travLnk error: outError];
 	if (!data) {
 		//The dataForKey:... method should have filled out the error variable.
@@ -233,7 +210,7 @@
 	}
 	NSPropertyListFormat	outFormat = NSPropertyListXMLFormat_v1_0;
 
-	id obj = [NSPropertyListSerialization propertyListWithData:data options:NSPropertyListImmutable format:&outFormat error:outError];
+	id obj = [NSPropertyListSerialization propertyListWithData: data options: NSPropertyListImmutable format: &outFormat error: outError];
 	if (!obj) {
 		//The propertyListWithData:... method should have filled out the error variable.
 		return nil;
@@ -243,15 +220,7 @@
 }
 
 
-// -----------------------------------------------------------------------------
-//	stringForKey:atPath:traverseLink:
-//		Retrieve the xattr with name key, which is an XML property list
-//		and unserialize it back into an object or object graph.
-//		path is the file whose xattr you want to set.
-//		If travLnk == YES, it follows symlinks.
-// -----------------------------------------------------------------------------
-
-+(NSString*) stringForKey: (NSString*)key atPath: (NSString*)path traverseLink:(BOOL)travLnk
++(nullable NSString*) stringForKey: (NSString*)key atPath: (NSString*)path traverseLink:(BOOL)travLnk
 {
 	NSData *data = [[self class] dataForKey: key atPath: path traverseLink: travLnk error: nil];
 	if (!data) {
@@ -261,8 +230,11 @@
 	return [[NSString alloc] initWithData: data encoding: NSUTF8StringEncoding];
 }
 
-+(NSString*) stringForKey: (NSString*)key atPath: (NSString*)path traverseLink:(BOOL)travLnk error:(NSError * _Nullable __autoreleasing * _Nullable)error
+
++(nullable NSString*) stringForKey: (NSString*)key atPath: (NSString*)path traverseLink:(BOOL)travLnk error:(NSError * _Nullable __autoreleasing * _Nullable)error
 {
+	NSAssert(key.length <= ULIMaxXAttrKeyLength, @"Key length limit exceeded.");
+	
 	NSData *data = [[self class] dataForKey: key atPath: path traverseLink: travLnk error: error];
 	
 	if (!data) {
@@ -285,7 +257,4 @@
 	return toRet;
 }
 
-
 @end
-
-#endif /*MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_4*/
